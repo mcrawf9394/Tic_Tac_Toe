3c4e7ffd5330d127e697416eb1486ac864b5915b
mcrawf9394 <mjc9394@gmail.com>
2024-03-25 20:24:52 -0400

    I made it to where there are buttons that interact with the game, and that a button cannot be selected twice. I still need to add a post game reset and the actual display for the won game.

--- script.js
+++ script.js
@@ -1,20 +1,51 @@
 const gameBoard = {
     gameBoard: [],
+    gameBoardContainer: document.getElementById('gameBoardContainer'),
     createGameBoard() {
         for (i = 0; i < 3; i++) {
             this.gameBoard.push([])
             for (j = 0; j <3; j++) {
-                this.gameBoard[i].push("")
+                this.gameBoard[i].push(" ")
             }
         }
         this.displayGameBoard(this.gameBoard)
     },
     updateGameBoard (i, j, userEntry){
         this.gameBoard[i][j] = userEntry
-        this.displayGameBoard(this.gameBoard)
+        let board = document.querySelectorAll('.grids')
+        board.forEach(cells => {
+            let value = cells.value
+            let row
+            let column
+            if (value < 3) {
+                row = 0
+                column = parseInt(value)
+            }
+            else if (value < 6 && value >= 3) {
+                row = 1
+                column = parseInt(value) - 3
+            }
+            else if (value >= 6){
+                row = 2
+                column = value - 6
+            }
+            cells.textContent = this.gameBoard[row][column]
+        });
     },
     displayGameBoard (board) {
-        console.log(board)
+        const gameBoardContainer = document.getElementById('gameBoardContainer')
+        m = 0
+        for (k = 0; k < 3; k++){
+            for(l = 0; l < 3; l++){
+                const newSpace = document.createElement('button')
+                newSpace.className = "grids"
+                newSpace.textContent = board[k][l]
+                newSpace.value = m
+                newSpace.addEventListener('click', () => playGame.turns(newSpace.value))
+                this.gameBoardContainer.appendChild(newSpace)
+                m++
+            }
+        }
     }
 }
 gameBoard.createGameBoard()
@@ -36,29 +67,38 @@ const playGame = {
         players.player1.choice = prompt("player 1 choice")
         players.player2.name = prompt("player 2 name")
         players.player2.choice = prompt("player 2 choice")
-        this.turns()
     },
-    turns () {
+    turns (value) {
+        let row
+        let column
+        if (value < 3) {
+            row = 0
+            column = parseInt(value)
+        }
+        else if (value < 6 && value >= 3) {
+            row = 1
+            column = parseInt(value) - 3
+        }
+        else if (value >= 6){
+            row = 2
+            column = value - 6
+        }
         if (this.first == this.second){
-            player1TurnRow = parseInt(prompt("row"))
-            player1TurnColumn = parseInt(prompt("column"))
             choice = players.player1.choice
-            this.turnHandling(player1TurnRow, player1TurnColumn, choice)
+            this.turnHandling(row, column, choice)
         }
         else {
-            player2TurnRow = prompt("row")
-            player2TurnColumn = prompt("column")
             choice2 = players.player2.choice
-            this.turnHandling(player2TurnRow, player2TurnColumn, choice2)
+            this.turnHandling(row, column, choice2)
         }
-        console.log(this.first)
     },
     turnHandling (row, column, choice){
-        if (gameBoard.gameBoard[row][column] != ""){
+        let board = gameBoard.gameBoard
+        if (board[row][column] != " "){
             alert("You Cannot place a x or o on a spot that has already been marked!")
-            this.turns()
-        }
-        else if (choice == "x"){
+            return
+        } 
+        else if (choice == players.player1.choice){
             this.first++
             gameBoard.updateGameBoard(row, column, choice)
         }
@@ -70,15 +110,25 @@ const playGame = {
         this.checkWinner(choice)
     },
     checkWinner (choice) {
+        isThereAWinner = false
         for (c = 0; c < 3; c++){
             if (gameBoard.gameBoard[c][0] === choice && gameBoard.gameBoard[c][1] === choice && gameBoard.gameBoard[c][2] === choice){
-                console.log("there's  a winner!")  
+                console.log("there's a winner!")
+                isThereAWinner = true
+                break
             }
             else if (gameBoard.gameBoard[0][c] === choice && gameBoard.gameBoard[1][c] === choice && gameBoard.gameBoard[2][c] === choice){
-                console.log("there's  a winner!")
+                console.log("there's a winner!")
+                isThereAWinner = true
+                break
+            }
+            else if (gameBoard.gameBoard[0][0] === choice && gameBoard.gameBoard[1][1] === choice && gameBoard.gameBoard[2][2] === choice || gameBoard.gameBoard[0][2] == choice && gameBoard.gameBoard[1][1] === choice && gameBoard.gameBoard[2][0]) {
+                console.log("there's a winner!")
+                isThereAWinner = true
+                break
             }
         }
-        this.turns()
+        return
     }
 }
 playGame.beginGame()
